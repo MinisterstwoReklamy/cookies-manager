3d6b67e3df217cfd402a6777ceab2dc7c3d7832e
unknown <ministerstworeklamy@gmail.com>
2025-07-24 14:48:12 +0200

    cookies24

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({ headless: 'false', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   const url = 'https://www.777ulotki.pl/';
 
   await page.goto(url, {waitUntil: 'load'});
 
-  await page.screenshot({path: 'mr-screenshot2.png'});
+  await page.waitForResponse(response =>
+    response.url().startsWith("https://cookies-manager.mr.org.pl/api/customer/")
+  );
 
+  const cookieAcceptBtn = await page.$("button[data-role='all']");
 
+  // await console.log('The element cookieAcceptBtn was resolved to: ' + cookieAcceptBtn);
+  await cookieAcceptBtn.click();  
+
+  console.log(await page.cookies());
 
   await browser.close();
 })();
