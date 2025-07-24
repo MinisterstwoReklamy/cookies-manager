81a0f9508b2bdc6afdbbd7118df0626a34a71254
unknown <ministerstworeklamy@gmail.com>
2025-07-24 13:15:16 +0200

    cookies

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://buddy.works');
-  await page.screenshot({ path: 'buddy-screenshot.png' });
+  await page.goto('https://www.ministerstworeklamy.pl/');
+  console.log(await browser.cookies());
 
   await browser.close();
 })();
