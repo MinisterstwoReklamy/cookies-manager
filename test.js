94305ccc32321d0ff491712b076f2ccb56af54b7
unknown <ministerstworeklamy@gmail.com>
2025-07-24 13:33:48 +0200

    cookies6

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://www.wp.pl/');
+  await page.goto('https://www.lukgood.pl/');
   console.log(await page.cookies());
 
   await browser.close();
