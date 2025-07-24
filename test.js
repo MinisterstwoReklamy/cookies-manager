66461e555b90aea20ba014edcb212843c52d24e9
unknown <ministerstworeklamy@gmail.com>
2025-07-24 13:46:40 +0200

    cookies7

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://www.lukgood.pl/');
+  await page.goto('https://www.777ulotki.pl/');
   console.log(await page.cookies());
 
   await browser.close();
