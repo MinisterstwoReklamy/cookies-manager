c958cfc1ddf02d16c8a8cd545c6339529277ee35
unknown <ministerstworeklamy@gmail.com>
2025-07-24 13:53:30 +0200

    cookies8

--- test.js
+++ test.js
@@ -3,7 +3,15 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
-  await page.goto('https://www.777ulotki.pl/');
+  const url = 'https://www.777ulotki.pl/';
+
+  await page.goto(url, {waitUntil: "domcontentloaded"});
+  const acceptBtnSelector = ">>> [class='cm__btn']";
+  console.log( acceptBtnSelector );
+  const accept = await page.waitForSelector(acceptBtnSelector);
+  await accept.click();
+  await page.waitForSelector(acceptBtnSelector, {hidden: true});
+
   console.log(await page.cookies());
 
   await browser.close();
