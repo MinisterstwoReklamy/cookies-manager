4837f9f7076aaf62b3fac801b37f685eb9ba46b2
unknown <ministerstworeklamy@gmail.com>
2025-07-24 14:42:03 +0200

    cookies22

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   const url = 'https://www.777ulotki.pl/';
 
-  await page.goto(url);
+  await page.goto(url, {waitUntil: 'networkidle0'});
 
-  await page.screenshot({path: 'mr-screenshot.png'});
+  await page.screenshot({path: 'mr-screenshot2.png'});
 
 
 
